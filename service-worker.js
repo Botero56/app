7a7fa0e946046c668d5e62dc4d3b4fb52dffa30a
Botero56 <boterosamuel3@gmail.com>
2026-03-16 12:28:57 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,26 @@
 const CACHE_NAME = "votaciones-app-v1";
 
 const urlsToCache = [
-  "/",
-  "index.html",
-  "rvotacion.html",
-  "style.css",
-  "style2.css",
-  "logo.png",
-  "icon-192.png",
-  "manifest.json"
+  "./",
+  "./index.html",
+  "./rvotacion.html",
+  "./style.css",
+  "./style2.css",
+  "./logo.png",
+  "./icon-192.png",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
